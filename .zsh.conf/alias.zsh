f66208ec17e778c430bd199498aa43c3b7f313e3
Ephemeral <theephemeral.txt@gmail.com>
2024-06-27 15:58:47 +0600

    split zsh config in multiple files

---
+++ .zsh.conf/alias.zsh
@@ -0,0 +1,71 @@
+alias vim=nvim
+alias n=pnpm
+alias pm=pm2
+alias yt=yt-dlp
+
+alias ffmpeg='ffmpeg -hide_banner'
+alias reload='clear && source ~/.zshrc'
+alias config='nvim ~/.zshrc'
+alias stdn='sudo shutdown now'
+alias svim='sudo -Es nvim'
+alias start="xdg-open"
+alias s.="xdg-open ."
+
+# LS alias (cargo install eza)
+alias cls='command ls'
+if command -v eza &>/dev/null; then
+	alias ls='eza --color=always --icons'
+	alias l='eza --color=always --icons -ial'
+	alias la='eza --color=always --icons -ia'
+	alias tree='eza --color=always --icons -ia --tree --git-ignore'
+else
+	echo "eza command is missing"
+fi
+
+# CD
+alias rd='cd -' # Return to previous directory
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+# Bat and Bat extra
+command -v bat &>/dev/null && alias cat=bat || echo "bat command is missing"
+command -v batman &>/dev/null && alias man=batman || echo "batman command is missing"
+command -v batdiff &>/dev/null && alias diff=batdiff || echo "batdiff command is missing"
+
+# GNU coreutils
+alias cp='cp -i'
+alias mv='mv -i'
+alias xrm='xargs rm'
+alias du='du -h'
+alias mkdir='mkdir -p'
+
+# Zellij
+alias za='zellij a $(zellij ls -n | fzf | cut -d" " -f1)'
+alias zr='zellij delete-session $(zellij ls | fzf | cut -d" " -f1)'
+alias zda='zellij delete-all-sessions'
+alias zn='zellij -s'
+
+# Fix ssh weirdness with kitty
+command -v kitty &>/dev/null && alias ssh="kitty +kitten ssh"
+
+# Sync dotfiles using GNU stow
+alias dotsync='stow -d ~/git/dotfiles/ -t ~/ .'
+
+# Git
+alias ga='git add'
+alias gp='git pull'
+alias gr='git reset'
+alias gcm='git commit'
+alias gp='git push'
+alias gaa='git add .'
+alias gt='git status'
+alias gd='batdiff || git diff'
+
+alias gpr='git pull --rebase'
+
+alias grh='git reset --hard'
+alias grs='git reset --soft'
+alias grhh='git reset --hard HEAD'
+alias grsh='git reset --soft HEAD'
---
+++ .zsh.conf/completion.zsh
@@ -0,0 +1,32 @@
+autoload -Uz compinit && compinit
+
+zinit cdreplay -q
+
+# History
+export HISTSIZE=5000
+export HISTFILE="$HOME/.local/share/zsh_history"
+export SAVEHIST=$HISTSIZE
+export HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' menu no
+
+# CD completion
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always $realpath'
+
+# Kill process completion
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags '--preview-window=down:3:wrap'
+
+# SYSTEMD
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
