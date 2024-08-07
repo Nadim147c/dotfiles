6b806ba5ab85968f82e5899f89cd73ff3c2b8376
Ephemeral <theephemeral.txt@gmail.com>
2024-08-07 21:40:21 +0600

    add checking for jsutils

--- .zsh.conf/exports.zsh
+++ .zsh.conf/exports.zsh
@@ -10,7 +10,7 @@ for item in ${CUSTOM_PATH[@]}; do
 	export PATH="$PATH:$item"
 done
 
-source "$HOME/git/jsutils/path"
+[[ -f "$HOME/git/jsutils/path" ]] && source "$HOME/git/jsutils/path"
 
 export FZF_DEFAULT_OPTS='
 --color=fg:#cdd6f4,header:#a6e3a1,info:#94e2d5,pointer:#f5e0dc
