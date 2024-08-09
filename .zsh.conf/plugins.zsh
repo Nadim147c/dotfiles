6cf2a74a3c9a88767924c0ebd7d38d3ef720cb8a
Ephemeral <theephemeral.txt@gmail.com>
2024-08-10 00:32:36 +0600

    add new plugins

--- .zsh.conf/plugins.zsh
+++ .zsh.conf/plugins.zsh
@@ -1,16 +1,20 @@
-autoload -U url-quote-magic
-zle -N self-insert url-quote-magic
-unsetopt nomatch
-
 # Add in zsh plugins
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
 zinit light djui/alias-tips
-zinit light Nadim147c/zsh-help
 zinit light MichaelAquilina/zsh-autoswitch-virtualenv
 zinit light zshzoo/cd-ls
+
+# My forks
+zinit light Nadim147c/zsh-help
+zinit light Nadim147c/zsh-archlinux
+
+# Completion Plugins
+zinit light zchee/zsh-completions
+zinit light clavelm/yt-dlp-omz-plugin
+zinit light redxtech/zsh-kitty
 
 # Add in snippets
 zinit snippet OMZP::sudo
