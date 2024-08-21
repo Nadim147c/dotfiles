467b1b47bd8130b9453aed10652acbf5acb809af
Ephemeral <theephemeral.txt@gmail.com>
2024-08-22 00:15:38 +0600

    remove fastfetch config update

--- .config/fastfetch/init.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -z "$ZELLIJ" && -z "$TMUX" ]]; then
-    fastfetch --logo-type kitty --logo "$(printf '%s\n' "$HOME/.config/fastfetch/logo/"* | shuf -n1)"
-fi
-
-alias fastfetch=$'fastfetch --logo-type kitty --logo "$(printf \'%s\n\' "$HOME/.config/fastfetch/logo/"* | shuf -n1)"'
