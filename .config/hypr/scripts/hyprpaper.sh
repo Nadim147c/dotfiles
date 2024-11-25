2499182e9d50bc0a327c65f30c9295cc3ff5fd27
Ephemeral <theephemeral.txt@gmail.com>
2024-11-25 19:16:24 +0600

    Hyprland stage 3

--- .config/hypr/scripts/hyprpaper.sh
+++ .config/hypr/scripts/hyprpaper.sh
@@ -1,17 +1,47 @@
 #!/bin/sh
 
+_hyprpaper_log() {
+    "$@" 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log
+}
+
+_hyprpaper_get_wallpaper() {
+    current_wallpaper=$(hyprctl hyprpaper listactive | awk 'NR==1' | grep -o '/.\+' || echo "<IGNORE_ME>")
+
+    find -L ~/Pictures/Wallpapers/ -type f |
+        grep -P '\.(jpg|jpeg|png)$' |
+        grep -Fv "$current_wallpaper" |
+        grep -Fv "/Pictures/Wallpapers/walpaper.jpg" |
+        shuf -n1
+}
+
+_hyprpaper_move_wallpaper() {
+    extension="${1##*.}"
+    if [ "$extension" = "jpg" ] || [ "$extension" = "jpeg" ]; then
+        cp "$1" ~/Pictures/Wallpapers/walpaper.jpg
+    else
+        magick "$1" ~/Pictures/Wallpapers/walpaper.jpg
+    fi
+}
+
+_hyprpaper_gen_colors() {
+    _hyprpaper_log echo "Generating color from '$1'"
+    _hyprpaper_log matugen image "$1" --show-colors
+}
+
+_hyprpaper_set_wallpaper() {
+    _hyprpaper_log hyprctl hyprpaper unload all
+    _hyprpaper_log hyprctl hyprpaper preload "$1"
+    _hyprpaper_log hyprctl hyprpaper wallpaper ",$1"
+}
+
+wallpaper=$(_hyprpaper_get_wallpaper)
+
+_hyprpaper_gen_colors "$wallpaper"
+
 if [ "$1" = "--startup" ]; then
-    hyprpaper 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log &
-    while hyprctl hyprpaper listloaded | grep -Pq "Couldn\'t connect to.+hyprpaper.sock"; do
-        sleep 0.1
-    done
+    _hyprpaper_move_wallpaper "$wallpaper" &
+    _hyprpaper_log hyprpaper &
+else
+    _hyprpaper_set_wallpaper "$wallpaper"
+    _hyprpaper_move_wallpaper "$wallpaper"
 fi
-
-current_wallpaper=$(hyprctl hyprpaper listactive | awk 'NR==1' | grep -o '/.\+')
-wallpaper=$(find -L ~/Pictures/Wallpapers/ -type f | grep -P '\.(jpg|jpeg|png)$' | grep -v "$current_wallpaper" | shuf -n1)
-
-matugen image "$wallpaper" 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log
-
-hyprctl hyprpaper unload all 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log
-hyprctl hyprpaper preload "$wallpaper" 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log
-hyprctl hyprpaper wallpaper ",$wallpaper" 2>&1 | tee -a ~/.local/share/logs/hyprpaper.log
