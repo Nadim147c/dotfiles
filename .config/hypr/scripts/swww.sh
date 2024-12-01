1539ead2c494e606bf24e396996af07e0cd35b2f
Ephemeral <theephemeral.txt@gmail.com>
2024-12-01 18:55:05 +0600

    Hyperland: partition 6

---
+++ .config/hypr/scripts/swww.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+_log() {
+    echo "$@" | logger -t 'swww-script'
+}
+_logger() {
+    "$@" 2>&1 | logger -t 'swww-script'
+}
+
+_get_wallpaper() {
+    current_wallpaper=$(swww query | grep -o '/.\+$' || echo "<IGNORE_ME>")
+
+    find -L ~/Pictures/Wallpapers/ -type f |
+        grep -P '\.(jpg|jpeg)$' |
+        grep -Fv "$current_wallpaper" |
+        grep -Fv "/Pictures/Wallpapers/walpaper.jpg" |
+        shuf -n1
+}
+
+_move_wallpaper() {
+    extension="${1##*.}"
+    if [ "$extension" = "jpg" ] || [ "$extension" = "jpeg" ]; then
+        cp "$1" ~/Pictures/Wallpapers/walpaper.jpg
+    else
+        magick "$1" ~/Pictures/Wallpapers/walpaper.jpg
+    fi
+}
+
+_gen_colors() {
+    _logger echo "Generating color from '$1'"
+    _logger matugen image "$1" --show-colors
+}
+
+_set_wallpaper() {
+    cursor_pos=$(hyprctl cursorpos | sed 's/ //')
+
+    if ! echo "$cursor_pos" | grep -Pq "^[0-9]+,[0-9]+$"; then
+        cursor_pos="0,0"
+    fi
+
+    _log "Setting wallpaper ($cursor_pos) '$1'"
+
+    _logger swww img \
+        --transition-type=grow \
+        --transition-fps=30 \
+        --invert-y --transition-pos="$cursor_pos" \
+        --transition-bezier='.43,1.19,1,.4' \
+        --transition-duration='0.6' \
+        "$1"
+}
+
+wallpaper=$(_get_wallpaper)
+
+_log "Selected wallpaper is $wallpaper"
+
+if [ "$1" = "--startup" ]; then
+    _log "Starting swww-daemon"
+    _logger swww-daemon
+    exit
+fi
+
+_set_wallpaper "$wallpaper" &
+_gen_colors "$wallpaper"
+_move_wallpaper "$wallpaper"
+sleep 4
