707f49c34218409021090f88cfd25eb47327fa0b
Ephemeral <theephemeral.txt@gmail.com>
2024-12-01 18:55:05 +0600

    Hyperland: partition 6

--- .config/hypr/scripts/waybar.sh
+++ .config/hypr/scripts/waybar.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+_log() {
+    echo "$@" | logger -t 'waybar-script'
+}
+_logger() {
+    "$@" 2>&1 | logger -t 'waybar-script'
+}
+
 trap "killall waybar" EXIT
 
 if ! command -v inotifywait >/dev/null 2>&1; then
-    waybar 2>&1 | tee -a ~/.local/share/logs/waybar.log
+    _logger waybar
     exit
 fi
 
-echo "Running waybar config daemon..."
-
+_log "Running waybar config daemon..."
 while true; do
-    waybar 2>&1 | tee -a ~/.local/share/logs/waybar.log &
-    inotifywait -e create,modify ~/.config/waybar/*
+    _log "Reloading waybar"
+    waybar &
+    inotifywait -e create,modify ~/.config/waybar/config.jsonc ~/.config/waybar/style.css
     killall waybar
 done
