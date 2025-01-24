29fa3fa3937a3b8e1aa626828087758500accb2a
Ephemeral <theephemeral.txt@gmail.com>
2025-01-24 10:57:24 +0600

    start spotify ad startup

--- .config/hypr/scripts/spicetify.sh
+++ .config/hypr/scripts/spicetify.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if pgrep -x spotify >/dev/null; then
-    if ! pgrep -f "spicetify watch -s" >/dev/null; then
-        hyprctl dispatch -- exec spicetify watch -s
-    fi
+if ! pgrep -f "spicetify watch -s" >/dev/null; then
+    hyprctl dispatch -- exec spicetify watch -s
 fi
