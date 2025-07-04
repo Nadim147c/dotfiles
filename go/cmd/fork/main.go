c718bf92a859857c71986bf58a149b09d44542f3
Ephemeral <theephemeral.txt@gmail.com>
2025-07-04 23:00:15 +0600

    add fork command that ensure launched command is forked

---
+++ go/cmd/fork/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <command> [args...]", os.Args[0])
+	}
+
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+
+	// Detach the process from the terminal/session
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+
+	// Redirect all stdio to /dev/null
+	devNull, err := os.OpenFile(os.DevNull, os.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("Failed to open /dev/null: %v", err)
+	}
+	defer devNull.Close()
+
+	cmd.Stdin = devNull
+	cmd.Stdout = devNull
+	cmd.Stderr = devNull
+
+	// Start the process (but do not wait)
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Failed to start process: %v", err)
+	}
+
+	// Exit immediately
+	os.Exit(0)
+}
