992fa8e4f392b6a4e8f492235589a2bcfe77e251
Ephemeral <theephemeral.txt@gmail.com>
2025-07-09 21:38:16 +0600

    update coffee.sh

--- .local/bin/coffee.sh
+++ .local/bin/coffee.sh
@@ -15,6 +15,8 @@ brew_notification() {
     fi
 }
 
+is_caffeinated() { [ -e "$COFFEE_FILE" ] && return 0 || return 1; }
+
 case "$1" in
 --check)
     if [ -e "$COFFEE_FILE" ]; then
@@ -34,8 +36,30 @@ case "$1" in
         brew_notification "Your computer has been caffeinated! It will stay awake."
     fi
     ;;
+--lock)
+    is_caffeinated && exit 0
+    # Use fork to ensure hyprlock doesn't die after locking the session
+    # 'fork' command can be found in
+    # https://github.com/Nadim147c/dotfiles/blob/ec6f6cf13eb50247d42803cf1de982b35c8f4cfa/go/cmd/fork/main.go
+    fork hyprlock
+    exit 0
+    ;;
+--sleep)
+    is_caffeinated && exit 0
+    systemctl suspend
+    exit 0
+    ;;
+--run)
+    is_caffeinated && exit 0
+    [ -n "$2" ] && {
+        shift
+        eval "$@"
+    }
+    exit 0
+    ;;
 *)
-    echo "Usage: $0 --check|--toggle" >&2
+    echo "Usage: $0 --check|--toggle|--lock|--sleep|--run \"command\""
+    brew_notification "Invalid command: $1"
     exit 1
     ;;
 esac
