b07a98ee422dc72bd71ba10b56d106f7c794fd1b
Ephemeral <theephemeral.txt@gmail.com>
2025-08-05 20:48:23 +0600

    nix: move shell based configuration to nix

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -9,6 +9,21 @@
         enableZshIntegration = true;
         enableBashIntegration = true;
     };
+
+    editor = "${pkgs.neovim}/bin/nvim";
+    lessCmd = "${pkgs.less}/bin/less -r -F";
+
+    # fix: for some reason home-manager converts shell variable from sh and set global variable to
+    # __HM_SESS_VARS_SOURCED=1 and check this variable before sourcing which makes fish ignore the
+    # all there shell variable
+    translatedSessionVariables = pkgs.runCommandLocal "hm-session-vars.fish" {} ''
+        (echo "function setup_hm_session_vars;"
+        echo "set __HM_SESS_VARS_SOURCED" # reset __HM_SESS_VARS_SOURCED so that shell var is set
+        ${pkgs.buildPackages.babelfish}/bin/babelfish \
+        <${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh
+        echo "end"
+        echo "setup_hm_session_vars") > $out
+    '';
 in {
     imports = [
         ./git.nix
@@ -37,6 +52,7 @@ in {
         sd
         skim
         yt-dlp
+        sccache
 
         chromashift
         git-sb
@@ -45,7 +61,73 @@ in {
     ];
 
     home.sessionVariables = {
+        XDG_CONFIG_HOME = config.xdg.configHome;
+        XDG_CACHE_HOME = config.xdg.cacheHome;
+        XDG_DATA_HOME = config.xdg.dataHome;
+        XDG_STATE_HOME = config.xdg.stateHome;
+
+        STARSHIP_CONFIG = "${config.xdg.configHome}/starship.toml";
+        WGETRC = "${config.xdg.configHome}/wget/config";
+        NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+
+        PNPM_HOME = "${config.xdg.dataHome}/pnpm";
+        CARGO_HOME = "${config.xdg.dataHome}/cargo";
+        RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+
+        # Cache paths
+        STARSHIP_CACHE = "${config.xdg.cacheHome}/starship";
+        GOMODCACHE = "${config.xdg.cacheHome}/go/mod";
+
+        # Editor settings
+        EDITOR = editor;
+        VISUAL = editor;
+
+        # Pager settings
+        LESS = "-r -F";
+        PAGER = lessCmd;
+        BAT_PAGER = lessCmd;
+        DELTA_PAGER = lessCmd;
+
+        # Color settings
+        GCC_COLORS = "error=1;31:warning=1;33:note=1;47;107:caret=1;47;107:locus=40;1;35:quote=1;33";
+        GREP_COLORS = ":mt=1;36:ms=41;1;30:mc=1;41:sl=:cx=:fn=1;35;40:ln=32:bn=32:se=1;36;40";
+
+        # LS_COLORS from vivid
         LS_COLORS = "$(${pkgs.vivid}/bin/vivid generate catppuccin-mocha)";
+
+        RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+    };
+
+    home.sessionPath = [
+        "${config.home.homeDirectory}/.local/bin"
+        "${config.xdg.dataHome}/cargo/bin"
+        "${config.xdg.dataHome}/pnpm"
+        "${config.xdg.cacheHome}/.bun/bin"
+    ];
+
+    home.shellAliases = {
+        # Aliases
+        zk = "zellij kill-all-sessions -y";
+
+        # Core utils aliases
+        du = "du -h";
+        grep = "grep --color";
+        less = "less -r -F";
+        exe = "chmod +x";
+        cat = "bat";
+
+        # Cd aliases
+        rd = "cd -";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../..";
+        "....." = "cd ../../../..";
+
+        # Other aliases
+        delta = "delta --line-numbers --hunk-header-decoration-style none";
+        ffmpeg = "ffmpeg -hide_banner";
+
+        gaa = "git add -A";
     };
 
     programs.go = {
@@ -85,25 +167,41 @@ in {
     };
     programs.fish = {
         enable = true;
-        shellInit = builtins.readFile ../static/fish/config.fish;
-        interactiveShellInit = ''
-            ${pkgs.chromashift}/bin/cshift alias fish | source
-        '';
+        shellInit = builtins.readFile "${translatedSessionVariables}";
+        interactiveShellInit =
+            # fish
+            ''
+                set -U fish_greeting
+                set fish_color_command blue --bold
+                set fish_color_redirection yellow --bold
+                set fish_color_option red
+                set fish_pager_color_prefix green --bold
+                set fish_pager_color_completion blue --bold
+                set fish_pager_color_description white --bold
+                # Functions
+                function field -a n
+                    awk "{ print \$$n }"
+                end
+
+                ${pkgs.chromashift}/bin/cshift alias fish | source
+            '';
         generateCompletions = false;
     };
 
     programs.fzf = {
         enable = true;
-        defaultOptions = ["--border" "--ansi" "--layout=reverse"];
+        defaultOptions = [
+            "--border"
+            "--ansi"
+            "--layout=reverse"
+        ];
         defaultCommand = "fd --type f --color=always";
     };
 
     home.file.".config/starship.toml".source = ../static/starship/starship.toml;
-    home.file.".config/atuin/config.toml".source = ../static/atuin/config.toml;
     programs.starship = commonShellIntegration;
     programs.carapace = commonShellIntegration;
     programs.mise = commonShellIntegration;
-    programs.atuin = commonShellIntegration;
 
     programs.zoxide = {
         enable = true;
