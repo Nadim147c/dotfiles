a61c0fe0f23eae00c3ec42991ecfd65b8bb198d7
Ephemeral <theephemeral.txt@gmail.com>
2025-08-07 14:08:38 +0600

    nix: add crop-image script

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,7 @@ final: prev: {
     wallpaper-sh = final.callPackage ../pkgs/wallpaper-sh.nix {};
     waybar-lyric = final.callPackage ../pkgs/waybar-lyric.nix {};
     chromashift = final.callPackage ../pkgs/chromashift.nix {};
+    crop-image = final.callPackage ../pkgs/crop-image.nix {};
     desktop-portal-starter = final.callPackage ../pkgs/desktop-portal-starter.nix {};
     go-modules = final.callPackage ../go {};
 }
