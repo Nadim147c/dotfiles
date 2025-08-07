765e0cd92e4ade99af1badc2ab0dfd26311f019a
Ephemeral <theephemeral.txt@gmail.com>
2025-08-07 10:43:23 +0600

    fastfetch: update fastfetch config

--- home/rice/fastfetch.nix
+++ home/rice/fastfetch.nix
@@ -1,65 +1,11 @@
-{pkgs, ...}: let
-    logo = ''
-        ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣿
-        ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
-        ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
-        ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
-        ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
-        ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
-        ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
-        ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
-        ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-        ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
-        ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
-        ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
-        ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣'';
-    logo-cmd = pkgs.writeShellScript "fastfetch-logo" ''
-        if [ -z "$TMUX" ] && [ -z "$ZELLIJ" ]; then
-            LOGO=$(${pkgs.fd}/bin/fd . --type f "$HOME/Pictures/fastfetch/" |
-        ${pkgs.coreutils}/bin/shuf -n1)
-        fi
-
-        if [ -n "$LOGO" ]; then
-            echo "$LOGO"
-            exit
-        fi
-
-        ASCII_LOGO=/tmp/fastfetch.logo
-        echo "${logo}" | ${pkgs.lolcat}/bin/lolcat -f -t > $ASCII_LOGO
-        echo $ASCII_LOGO
-    '';
-in {
+{...}: {
     programs.fastfetch = {
         enable = true;
         settings = {
-            logo = {
-                source = "$(${logo-cmd})";
-                height = 13;
-            };
-            general = {
-                multithreading = true;
-            };
-            display = {
-                separator = " : ";
-            };
+            logo.type = "small";
+            general.multithreading = true;
+            display.separator = " : ";
             modules = [
-                "break"
-                {
-                    type = "cpu";
-                    key = "  CPU";
-                    keyColor = "red";
-                }
-                {
-                    type = "gpu";
-                    key = "  GPU";
-                    keyColor = "blue";
-                }
-                {
-                    type = "memory";
-                    key = "  RAM";
-                    keyColor = "green";
-                }
-                "break"
                 {
                     type = "title";
                     key = "  SYS";
@@ -76,7 +22,6 @@ in {
                     key = "  KER";
                     keyColor = "green";
                 }
-                "break"
                 {
                     type = "packages";
                     key = "  PAC";
@@ -88,11 +33,8 @@ in {
                     keyColor = "blue";
                 }
                 {
-                    type = "terminal";
-                    key = "  TRM";
-                    keyColor = "green";
+                    type = "colors";
                 }
-                "break"
             ];
         };
     };
