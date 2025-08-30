7995ff80955cbf079ef5d001b2fca34b8b8650d2
Ephemeral <theephemeral.txt@gmail.com>
2025-08-30 20:05:15 +0600

    nix: more nix stuff...

---
+++ modules/programs/yt-dlp.nix
@@ -0,0 +1,25 @@
+{delib, ...}:
+delib.module {
+    name = "programs.yt-dlp";
+
+    options = delib.singleEnableOption true;
+
+    home.ifEnabled.programs.yt-dlp = {
+        enable = true;
+        settings = {
+            paths = "~/Downloads/Videos";
+            format = "(bv[height<=1080]+ba)/(b[height<=1080])/b";
+            output = "%(title)s-%(id)s.%(ext)s";
+            merge-output-format = "mp4";
+            concurrent-fragments = 4;
+            sponsorblock-mark = "all";
+            sub-langs = "all";
+            embed-subs = true;
+            embed-thumbnail = true;
+            embed-metadata = true;
+            embed-chapters = true;
+            color = "always";
+            no-simulate = true;
+        };
+    };
+}
