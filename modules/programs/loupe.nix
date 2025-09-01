3b399f5c3ada86a6b9270ec1609c8657d15792b7
Ephemeral <theephemeral.txt@gmail.com>
2025-09-02 00:14:04 +0600

    loupe: add loupe config

---
+++ modules/programs/loupe.nix
@@ -0,0 +1,11 @@
+{
+    delib,
+    host,
+    pkgs,
+    ...
+}:
+delib.module {
+    name = "programs.loupe";
+    options = delib.singleEnableOption host.isDesktop;
+    home.ifEnabled.home.packages = [pkgs.loupe];
+}
