9e53878f12e72b5e4ad7bb808047a9ca0f72c6cb
Ephemeral <theephemeral.txt@gmail.com>
2025-09-04 17:51:08 +0600

    tty: configure tty console

---
+++ modules/setup/console.nix
@@ -0,0 +1,28 @@
+{delib, ...}:
+delib.module {
+    name = "setup.console";
+
+    options = delib.singleEnableOption true;
+
+    nixos.ifEnabled.console = {
+        enable = true;
+        colors = [
+            "000000"
+            "f38ba8"
+            "a6e3a1"
+            "f9e2af"
+            "89b4fa"
+            "f5c2e7"
+            "94e2d5"
+            "a6adc8"
+            "000000"
+            "f38ba8"
+            "a6e3a1"
+            "f9e2af"
+            "89b4fa"
+            "f5c2e7"
+            "94e2d5"
+            "bac2de"
+        ];
+    };
+}
