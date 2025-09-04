4fadb5946513e5d7cef674b72ad5e42e8a404ee6
Ephemeral <theephemeral.txt@gmail.com>
2025-09-04 10:55:57 +0600

    nixos: fix wrong locale

--- hosts/chronoshift/default.nix
+++ hosts/chronoshift/default.nix
@@ -19,23 +19,9 @@ delib.host {
 
         networking.networkmanager.enable = true;
 
-        # Set your time zone.
         time.timeZone = "Asia/Dhaka";
-
-        # Select internationalisation properties.
         i18n.defaultLocale = "en_US.UTF-8";
-
-        i18n.extraLocaleSettings = {
-            LC_ADDRESS = "es_US.UTF-8";
-            LC_IDENTIFICATION = "es_US.UTF-8";
-            LC_MEASUREMENT = "es_US.UTF-8";
-            LC_MONETARY = "es_US.UTF-8";
-            LC_NAME = "es_US.UTF-8";
-            LC_NUMERIC = "es_US.UTF-8";
-            LC_PAPER = "es_US.UTF-8";
-            LC_TELEPHONE = "es_US.UTF-8";
-            LC_TIME = "es_US.UTF-8";
-        };
+        i18n.extraLocaleSettings.LC_ALL = "en_US.UTF-8";
 
         # Configure keymap in X11
         services.xserver.xkb = {
