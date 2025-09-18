d1e313bb5b1935cbad54cdf3ff3953763a36f519
Ephemeral <theephemeral.txt@gmail.com>
2025-09-18 09:51:39 +0600

    btop: add btop settings

--- modules/programs/btop.nix
+++ modules/programs/btop.nix
@@ -2,5 +2,12 @@
 delib.module {
     name = "programs.btop";
     options = delib.singleEnableOption true;
-    home.ifEnabled.programs.btop.enable = true;
+    home.ifEnabled.programs.btop = {
+        enable = true;
+        settings = {
+            theme_background = false;
+            vim_keys = true;
+            update_ms = 200;
+        };
+    };
 }
