e5f7892012be43f27c505ba00da1f4b88ae6056f
Ephemeral <theephemeral.txt@gmail.com>
2025-09-20 13:03:10 +0600

    nix: flake update

--- flake.nix
+++ flake.nix
@@ -2,30 +2,33 @@
     description = "Modular configuration of NixOS, Home Manager, and Nix-Darwin with Denix";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager/master";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nix-darwin = {
-            url = "github:nix-darwin/nix-darwin/master";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+        catppuccin.url = "github:catppuccin/nix";
         denix = {
             url = "github:yunfachi/denix";
             inputs.nixpkgs.follows = "nixpkgs";
             inputs.home-manager.follows = "home-manager";
             inputs.nix-darwin.follows = "nix-darwin";
         };
-        zen-browser = {
-            url = "github:0xc000022070/zen-browser-flake";
+        home-manager = {
+            url = "github:nix-community/home-manager/master";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         hyprland.url = "github:hyprwm/Hyprland";
         hyprqt6engine.url = "github:hyprwm/hyprqt6engine";
-        catppuccin.url = "github:catppuccin/nix";
+        nix-darwin = {
+            url = "github:nix-darwin/nix-darwin/master";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
         nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-        rong.url = "github:Nadim147c/rong";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        rong = {
+            url = "github:Nadim147c/rong";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        zen-browser = {
+            url = "github:0xc000022070/zen-browser-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
     outputs = {denix, ...} @ inputs: let
