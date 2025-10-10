93c3558f294985cdc1fcc0da18eb4c138a9e00dc
Ephemeral <theephemeral.txt@gmail.com>
2025-10-10 17:35:01 +0600

    fork: format fork scripts

--- pkgs/fork.nix
+++ pkgs/fork.nix
@@ -8,12 +8,15 @@ writeShellApplication {
     runtimeInputs = [uwsm];
     text = ''
         if [ $# -eq 0 ]; then
-          echo "Usage: fork <executable|entry.desktop[:action]> [args...]" >&2
-          exit 1
+            echo "Usage: fork <executable|entry.desktop[:action]> [args...]" >&2
+            exit 1
         fi
 
         # Fork a new process that runs uwsm with the given args
-        (setsid uwsm app -- "$@" >/dev/null 2>&1 & disown) &
+        (
+            setsid uwsm app -- "$@" >/dev/null 2>&1 &
+            disown
+        ) &
         exit 0
     '';
 }
