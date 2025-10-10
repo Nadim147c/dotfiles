07b85ade41a8b6f65e2dfc3c27263dd96daa4b9b
Ephemeral <theephemeral.txt@gmail.com>
2025-10-10 17:28:42 +0600

    wtf: refactor and format

--- pkgs/wtf.nix
+++ pkgs/wtf.nix
@@ -7,20 +7,16 @@
 }:
 writeShellApplication {
     name = "wtf";
-    runtimeInputs = [
-        ripgrep
-        coreutils
-        man
-    ];
+    runtimeInputs = [coreutils man ripgrep];
     text = ''
         if [ $# -lt 2 ] || [ "$1" != "is" ]; then
-          echo "Usage: wtf is <query>" >&2
-          exit 1
+            echo "Usage: wtf is <query>" >&2
+            exit 1
         fi
 
-        shift  # drop "is"
+        shift # drop "is"
         query=$(printf "%s-" "$@" | sed 's/-$//')
 
-        man -k "$query" | rg "^$query" | head -n1 | sed 's|\s\+| |' | xargs echo
+        man -k "$query" | rg "^$query" | head -n1 | xargs
     '';
 }
