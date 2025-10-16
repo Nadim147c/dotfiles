901ad100ef63cd5c9190c2a02c24345e9cdb809f
Ephemeral <theephemeral.txt@gmail.com>
2025-10-16 22:14:45 +0600

    zsh: use xdg parameter instead of config

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -1,8 +1,8 @@
 {
-    config,
     delib,
     lib,
     pkgs,
+    xdg,
     ...
 }:
 delib.module {
@@ -10,19 +10,14 @@ delib.module {
 
     options = delib.singleEnableOption true;
 
-    home.ifEnabled = {myconfig, ...}: let
-        inherit (myconfig.constants) username;
-        xdg = config.home-manager.users.${username}.xdg;
-    in {
-        programs.zsh = {
-            enable = true;
-            enableCompletion = true;
-            autosuggestion.enable = true;
-            history.path = "${xdg.dataHome}/zsh/history";
-            history.ignoreSpace = true;
-            initContent = lib.mkOrder 1200 ''
-                source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
-            '';
-        };
+    home.ifEnabled.programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        history.path = "${xdg.dataHome}/zsh/history";
+        history.ignoreSpace = true;
+        initContent = lib.mkOrder 1200 ''
+            source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+        '';
     };
 }
