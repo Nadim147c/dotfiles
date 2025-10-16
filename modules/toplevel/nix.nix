1e43a92878e1171472ced1790cbae1f65caf8f0f
Ephemeral <theephemeral.txt@gmail.com>
2025-10-16 20:47:24 +0600

    nix: add nix config

---
+++ modules/toplevel/nix.nix
@@ -0,0 +1,20 @@
+{
+    delib,
+    pkgs,
+    lib,
+    ...
+}: let
+    shared.nix = {
+        package = lib.mkForce pkgs.nixVersions.latest;
+        settings = {
+            experimental-features = ["nix-command" "flakes"];
+            trusted-users = ["root" "@wheel"];
+            warn-dirty = false;
+        };
+    };
+in
+    delib.module {
+        name = "nix";
+        nixos.always = shared;
+        home.always = shared;
+    }
