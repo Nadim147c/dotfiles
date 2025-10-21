3df1ec62497232b2d7b289f8e1ee3a321ce56d9b
Ephemeral <theephemeral.txt@gmail.com>
2025-10-21 09:56:35 +0600

    mpvpaper: sort the package list

--- modules/programs/mpvpaper.nix
+++ modules/programs/mpvpaper.nix
@@ -8,7 +8,11 @@ delib.module {
     name = "programs.mpvpaper";
     options = delib.singleEnableOption host.isDesktop;
     home.ifEnabled = {
-        home.packages = with pkgs; [mpvpaper mpvpaper-daemon wallpaper-sh];
+        home.packages = with pkgs; [
+            mpvpaper
+            mpvpaper-daemon
+            wallpaper-sh
+        ];
         wayland.windowManager.hyprland.settings.exec-once = [
             "${pkgs.uwsm}/bin/uwsm app -- ${pkgs.mpvpaper-daemon}/bin/mpvpaper-daemon"
         ];
