f0e1a41f8cc33fb40dcfc67aaf53293283b2cba4
Ephemeral <theephemeral.txt@gmail.com>
2025-11-02 19:37:29 +0600

    ghostty: add ghostty config

---
+++ modules/programs/ghostty.nix
@@ -0,0 +1,39 @@
+{
+    constants,
+    delib,
+    host,
+    pkgs,
+    ...
+}:
+delib.module {
+    name = "programs.ghostty";
+
+    options = delib.singleEnableOption host.isPC;
+
+    home.ifEnabled.programs.ghostty = {
+        enable = true;
+        package = pkgs.ghostty;
+        enableZshIntegration = true;
+        enableFishIntegration = false;
+        enableBashIntegration = false;
+        installVimSyntax = true;
+        installBatSyntax = true;
+
+        settings = {
+            theme = "rong";
+            font-family = "JetBrainsMono Nerd Font";
+            font-size = 10;
+            command = constants.shell;
+            window-padding-x = 5;
+            window-padding-y = 5;
+            bold-is-bright = false;
+            window-vsync = true;
+        };
+
+        # themes = {
+        #     colors = {
+        #         include = "colors.conf";
+        #     };
+        # };
+    };
+}
