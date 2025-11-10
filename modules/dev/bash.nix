c90101308c093ccb42ae2decedc22c6a178ef110
Ephemeral <theephemeral.txt@gmail.com>
2025-11-10 21:19:15 +0600

    bash: add bash dev modules

---
+++ modules/dev/bash.nix
@@ -0,0 +1,18 @@
+{
+    delib,
+    host,
+    pkgs,
+    ...
+}:
+delib.module {
+    name = "dev.bash";
+
+    options = delib.singleEnableOption host.devFeatured;
+
+    home.ifEnabled.home.packages = with pkgs; [
+        bash-language-server
+        shfmt
+        shellcheck
+        shellharden
+    ];
+}
