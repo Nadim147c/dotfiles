5565dde7948fed0bbf0ad1a7e619eeffec3914e1
Ephemeral <theephemeral.txt@gmail.com>
2025-11-15 15:17:08 +0600

    nix: add writers.writeGo[Bin] module

---
+++ overlays/go-writers.nix
@@ -0,0 +1,23 @@
+{delib, ...}:
+delib.overlayModule {
+    name = "go-writers";
+    overlay = final: prev: rec {
+        writeGo = name: {
+            makeWrapperArgs ? [],
+            go ? prev.go,
+            goArgs ? [],
+            strip ? true,
+        }:
+            prev.writers.makeBinWriter {
+                compileScript = ''
+                    cp "$contentPath" tmp.go
+                    export HOME=$NIX_BUILD_TOP/.home
+                    ${go}/bin/go build ${prev.lib.escapeShellArgs goArgs} -o "$out" tmp.go
+                '';
+                inherit makeWrapperArgs strip;
+            }
+            name;
+
+        writeGoBin = name: writeGo "/bin/${name}";
+    };
+}
