f9d930f51909d1208a8571c2443cf6c1336bb9bd
Ephemeral <theephemeral.txt@gmail.com>
2025-11-17 15:52:47 +0600

    gtk+qt: add toplevel gtk and qt modules

--- modules/programs/hyprland/default.nix
+++ modules/programs/hyprland/default.nix
@@ -13,17 +13,8 @@ delib.module {
         programs.hyprland.withUWSM = true;
         programs.hyprland.enable = true;
         environment.variables = {
-            QT_QPA_PLATFORM = "wayland;xcb";
-            QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-            QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-            QT_QPA_PLATFORMTHEME = "qt6ct";
-
-            GTK_THEME = "adw-gtk3-dark";
-
-            GDK_BACKEND = "wayland,x11,*";
             SDL_VIDEODRIVER = "wayland";
             CLUTTER_BACKEND = "wayland";
-
             XDG_MENU_PREFIX = "arch-";
             XDG_CURRENT_DESKTOP = "Hyprland";
             XDG_SESSION_TYPE = "wayland";
---
+++ modules/toplevel/qt.nix
@@ -0,0 +1,30 @@
+{
+    delib,
+    pkgs,
+    host,
+    ...
+}:
+delib.module {
+    name = "qt";
+
+    options = delib.singleEnableOption host.guiFeatured;
+
+    nixos.ifEnabled.environment.variables = {
+        QT_QPA_PLATFORM = "wayland;xcb";
+        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+        QT_QPA_PLATFORMTHEME = "qt6ct";
+    };
+
+    home.ifEnabled = {
+        home.packages = with pkgs; [
+            libsForQt5.qt5ct
+            qt6Packages.qt6ct
+        ];
+
+        qt = {
+            enable = true;
+            platformTheme.name = "qt6ct";
+        };
+    };
+}
