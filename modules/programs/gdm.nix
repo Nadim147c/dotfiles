07dd93440e011d1b4101dcd3af541711fc2274ed
Ephemeral <theephemeral.txt@gmail.com>
2025-11-25 18:58:36 +0600

    gdm: use gdm as default display (login) manager

---
+++ modules/programs/gdm.nix
@@ -0,0 +1,27 @@
+{
+    constants,
+    delib,
+    host,
+    ...
+}:
+delib.module {
+    name = "programs.gdm";
+
+    options = delib.singleEnableOption host.isDesktop;
+
+    nixos.ifEnabled.services.displayManager = {
+        enable = true;
+        defaultSession = "hyprland-uwsm";
+        autoLogin = {
+            enable = true;
+            user = constants.username;
+        };
+        gdm = {
+            enable = true;
+            wayland = true;
+            autoSuspend = true;
+            autoLogin.delay = 0;
+        };
+        sddm.enable = false;
+    };
+}
