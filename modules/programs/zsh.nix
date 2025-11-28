37720871da799e54976800bba273ba3a075dd5c1
Ephemeral <theephemeral.txt@gmail.com>
2025-11-28 21:50:47 +0600

    zsh: remove zinit

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -10,36 +10,29 @@ delib.module {
 
     options = delib.singleEnableOption true;
 
-    home.ifEnabled.home.packages = with pkgs; [nix-zsh-completions];
     home.ifEnabled.programs.zsh = {
         enable = true;
-        enableCompletion = true;
+        enableCompletion = false;
         history.path = "${xdg.dataHome}/zsh/history";
         history.ignoreSpace = true;
         completionInit = "";
-        initContent = let
-            plugins = with pkgs; [
-                "${zsh-fzf-tab}/share/fzf-tab"
-                "${zsh-fast-syntax-highlighting}/share/zsh/site-functions"
-                "${zsh-autosuggestions}/share/zsh-autosuggestions"
-            ];
-            joinedPlugins = lib.concatStringsSep " " plugins;
-        in
-            lib.mkOrder 500 # bash
-            
-            ''
-                source ${pkgs.zinit}/share/zinit/zinit.zsh
-                zinit light-mode lucid wait for ${joinedPlugins}
+        initContent = lib.mkOrder 500 # bash
 
-                zstyle ':completion:*:git-checkout:*' sort false
-                zstyle ':completion:*:descriptions' format '[%d]'
-                zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
-                zstyle ':completion:*' menu no
-                zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-                zstyle ':fzf-tab:*' fzf-flags --bind=tab:accept --border=none
-                zstyle ':fzf-tab:*' use-fzf-default-opts yes
-                zstyle ':fzf-tab:*' switch-group '<' '>'
-                zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-            '';
+        ''
+            source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+            source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+            source ${pkgs.zsh-autosuggestions}/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+
+            zstyle ':completion:*:git-checkout:*' sort false
+            zstyle ':completion:*:descriptions' format '[%d]'
+            zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+            zstyle ':completion:*' menu no
+            zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+            zstyle ':fzf-tab:*' fzf-flags --bind=tab:accept --border=none
+            zstyle ':fzf-tab:*' use-fzf-default-opts yes
+            zstyle ':fzf-tab:*' switch-group '<' '>'
+            zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+            zstyle ':fzf-tab:*' popup-min-size 80 12
+        '';
     };
 }
