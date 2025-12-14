e9f88635b8351797c57f0e82ed721f904c0b4281
Ephemeral <theephemeral.txt@gmail.com>
2025-12-14 13:05:31 +0600

    nix-ld: add list of libraries

--- modules/toplevel/nix-ld.nix
+++ modules/toplevel/nix-ld.nix
@@ -1,6 +1,30 @@
-{delib, ...}:
+{
+    delib,
+    pkgs,
+    ...
+}:
 delib.module {
     name = "nix-ld";
     options = delib.singleEnableOption true;
-    nixos.ifEnabled.programs.nix-ld.enable = true;
+    nixos.ifEnabled.programs.nix-ld = {
+        enable = true;
+        libraries = with pkgs; [
+            acl
+            attr
+            bzip2
+            curl
+            libsodium
+            libssh
+            libxml2
+            libz
+            openssl
+            stdenv.cc.cc
+            stdenv.cc.cc.lib
+            systemd
+            util-linux
+            xz
+            zlib
+            zstd
+        ];
+    };
 }
