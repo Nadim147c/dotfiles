6df2028525b16f54099603baa7cbb50878101d4a
Ephemeral <theephemeral.txt@gmail.com>
2025-12-14 13:05:13 +0600

    udisk2: add udisk2 config

---
+++ modules/programs/udisk2.nix
@@ -0,0 +1,13 @@
+{
+    delib,
+    host,
+    ...
+}:
+delib.module {
+    name = "programs.udisks2";
+    options = delib.singleEnableOption host.isPC;
+    nixos.ifEnabled.services.udisks2 = {
+        enable = true;
+        mountOnMedia = true;
+    };
+}
