c90031c756da88bcae5d9ced4c882ff30658af5a
Ephemeral <theephemeral.txt@gmail.com>
2025-12-21 22:18:39 +0600

    freeze: use func utilities

--- modules/programs/freeze.nix
+++ modules/programs/freeze.nix
@@ -3,6 +3,7 @@
   host,
   pkgs,
   xdg,
+  func,
   ...
 }:
 delib.module {
@@ -10,12 +11,10 @@ delib.module {
   # Screenshot tool only makes sense in personal computer
   options = delib.singleEnableOption host.isPC;
 
-  home.ifEnabled.home.packages =
-    let
-      pkg = pkgs.writeShellScriptBin "freeze" ''
-        exec -a "$0" ${pkgs.charm-freeze}/bin/freeze \
-            --output ${xdg.userDirs.pictures}/"freeze-$(date +'%Y-%m-%d_%H-%M-%S').png" "$@"
-      '';
-    in
-    [ pkg ];
+  home.ifEnabled.home.packages = func.mkList (
+    pkgs.writeShellScriptBin "freeze" ''
+      exec -a "$0" ${pkgs.charm-freeze}/bin/freeze \
+          --output ${xdg.userDirs.pictures}/"freeze-$(date +'%Y-%m-%d_%H-%M-%S').png" "$@"
+    ''
+  );
 }
