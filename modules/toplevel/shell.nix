a802342c7e2c66f87a1bf9dea450518443ad65fb
Ephemeral <theephemeral.txt@gmail.com>
2025-12-21 22:21:11 +0600

    toplevel/shell: change module style

--- modules/toplevel/shell.nix
+++ modules/toplevel/shell.nix
@@ -9,83 +9,78 @@ delib.module {
   name = "shell";
   options = delib.singleEnableOption true;
 
-  home.ifEnabled =
-    let
-      editor = "${pkgs.neovim}/bin/nvim";
-      lessCmd = "${pkgs.less}/bin/less -r -F";
-    in
-    {
-      home.packages = with pkgs; [
-        btop
-        chafa
-        cmatrix
-        coreutils-full
-        ffmpeg
-        file
-        findutils
-        fork
-        fzf
-        gallery-dl
-        gum
-        jq
-        killall
-        less
-        nitch
-        openssl
-        perf
-        procps
-        xxHash
-        xxd
-        yq
-      ];
+  home.ifEnabled = {
+    home.packages = with pkgs; [
+      btop
+      chafa
+      cmatrix
+      coreutils-full
+      ffmpeg
+      file
+      findutils
+      fork
+      fzf
+      gallery-dl
+      gum
+      jq
+      killall
+      less
+      nitch
+      openssl
+      perf
+      procps
+      xxHash
+      xxd
+      yq
+    ];
 
-      home.sessionVariables = {
-        # Editor settings
-        EDITOR = editor;
-        VISUAL = editor;
-
+    home.sessionVariables =
+      let
+        less = "${pkgs.less}/bin/less -r -F";
+      in
+      {
         # Pager settings
         LESS = "-r -F";
-        DELTA_PAGER = lessCmd;
-        PAGER = lessCmd;
+        DELTA_PAGER = less;
+        PAGER = less;
 
         # Color settings
         GCC_COLORS = "error=1;31:warning=1;33:note=1;47;107:caret=1;47;107:locus=40;1;35:quote=1;33";
         GREP_COLORS = ":mt=1;36:ms=41;1;30:mc=1;41:sl=:cx=:fn=1;35;40:ln=32:bn=32:se=1;36;40";
       };
 
-      home.sessionPath = [
-        "${home.home.homeDirectory}/.local/bin"
-        "${xdg.dataHome}/cargo/bin"
-        "${xdg.dataHome}/pnpm"
-        "${xdg.cacheHome}/.bun/bin"
-        "${xdg.dataHome}/go/bin"
-      ];
+    home.sessionPath = [
+      "${home.home.homeDirectory}/.local/bin"
+      "${xdg.dataHome}/cargo/bin"
+      "${xdg.dataHome}/pnpm"
+      "${xdg.cacheHome}/.bun/bin"
+      "${xdg.dataHome}/go/bin"
+    ];
 
-      home.shellAliases = {
-        # Core utils aliases
-        du = "env du -h";
-        grep = "env grep --color";
-        exe = "chmod +x";
+    home.shellAliases = {
+      # Core utils aliases
+      du = "env du -h";
+      grep = "env grep --color";
+      exe = "chmod +x";
 
-        x = "xargs";
+      x = "xargs";
 
-        # Cd aliases
-        rd = "cd -";
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        "...." = "cd ../../..";
-        "....." = "cd ../../../..";
+      # Cd aliases
+      rd = "cd -";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
 
-        sctl = "systemctl";
-        uctl = "systemctl --user";
+      sctl = "systemctl";
+      uctl = "systemctl --user";
 
-        # Other aliases
-        delta = "${pkgs.delta}/bin/delta --line-numbers --hunk-header-decoration-style none";
-        ffmpeg = "${pkgs.ffmpeg}/bin/ffmpeg -hide_banner";
+      # Other aliases
+      delta = "${pkgs.delta}/bin/delta --line-numbers --hunk-header-decoration-style none";
+      ffmpeg = "${pkgs.ffmpeg}/bin/ffmpeg -hide_banner";
 
-        gaa = "git add -A";
-        tk = "tmux ls | cut -d: -f1 | xargs -r -n1 tmux kill-session -t";
-      };
+      gaa = "git add -A";
+      tk = "tmux ls | cut -d: -f1 | xargs -r -n1 tmux kill-session -t";
     };
+  };
 }
