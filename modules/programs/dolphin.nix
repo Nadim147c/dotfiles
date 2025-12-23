f6f8deea269962d91d1eea5230fd3f2f00fa22f5
Ephemeral <theephemeral.txt@gmail.com>
2025-12-24 00:59:02 +0600

    dolphin: update module

--- modules/programs/dolphin.nix
+++ modules/programs/dolphin.nix
@@ -5,42 +5,33 @@
   pkgs,
   ...
 }:
+let
+  application-menu = pkgs.runCommandLocal "xdg-application-menu" { } ''
+    mkdir -p $out/etc/xdg/menus/
+    ln -s ${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu $out/etc/xdg/menus/applications.menu
+  '';
+in
 delib.module {
   name = "programs.dolphin";
   options = delib.singleEnableOption host.guiFeatured;
   home.ifEnabled = {
-    home.packages =
-      with pkgs.kdePackages;
-      let
-        application-menu = pkgs.runCommandLocal "xdg-application-menu" { } ''
-          mkdir -p $out/etc/xdg/menus/
-          ln -s ${plasma-workspace}/etc/xdg/menus/plasma-applications.menu $out/etc/xdg/menus/applications.menu
-        '';
-      in
-      [
-        application-menu
-        dolphin
-        ffmpegthumbs
-        kdegraphics-thumbnailers
-        kio-admin
-        kio-extras
-        qtsvg
-      ];
+    home.packages = with pkgs.kdePackages; [
+      application-menu
+      dolphin
+      ffmpegthumbs
+      kdegraphics-thumbnailers
+      kio-admin
+      kio-extras
+      qtsvg
+    ];
 
     wayland.windowManager.hyprland.settings = {
       "$files" = func.wrapUWSM "${pkgs.kdePackages.dolphin}/bin/dolphin";
     };
 
     xdg.configFile."dolphinrc".text = ''
-      [KFileDialog Settings]
-      Places Icons Auto-resize=false
-      Places Icons Static Size=22
-
       [UiSettings]
       ColorScheme=Rong
-
-      [PreviewSettings]
-      Plugins=appimagethumbnail,audiothumbnail,comicbookthumbnail,cursorthumbnail,djvuthumbnail,ebookthumbnail,exrthumbnail,imagethumbnail,jpegthumbnail,kraorathumbnail,opendocumentthumbnail,svgthumbnail,windowsexethumbnail,windowsimagethumbnail,blenderthumbnail,ffmpegthumbs,gsthumbnail,mltpreview,mobithumbnail,rawthumbnail
     '';
   };
 }
