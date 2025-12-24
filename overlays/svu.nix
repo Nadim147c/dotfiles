c3009e79d7f31f91d4c14408d2f2041ff38e03c9
Ephemeral <theephemeral.txt@gmail.com>
2025-12-24 14:59:52 +0600

    overlays/svu: format svu overlay

--- overlays/svu.nix
+++ overlays/svu.nix
@@ -5,14 +5,15 @@ delib.overlayModule {
     svu = prev.svu.overrideAttrs (old: {
       nativeBuildInputs = old.nativeBuildInputs ++ [ prev.installShellFiles ];
       postInstall =
-        prev.lib.optionalString (prev.stdenv.buildPlatform.canExecute prev.stdenv.hostPlatform) # bash
-
-          ''
-            installShellCompletion --cmd svu \
-              --bash <($out/bin/svu completion bash) \
-              --fish <($out/bin/svu completion fish) \
-              --zsh <($out/bin/svu completion zsh)
-          '';
+        let
+          canExecute = prev.stdenv.buildPlatform.canExecute prev.stdenv.hostPlatform;
+        in
+        prev.lib.optionalString canExecute /* bash */ ''
+          installShellCompletion --cmd svu \
+            --bash <($out/bin/svu completion bash) \
+            --fish <($out/bin/svu completion fish) \
+            --zsh <($out/bin/svu completion zsh)
+        '';
     });
   };
 }
