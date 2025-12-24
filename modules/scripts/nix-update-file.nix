b790816aff3808530e3a31ad1b536dde56924eb2
Ephemeral <theephemeral.txt@gmail.com>
2025-12-24 14:59:35 +0600

    nix-update-file: add --unstable flag

--- modules/scripts/nix-update-file.nix
+++ modules/scripts/nix-update-file.nix
@@ -6,8 +6,12 @@
 delib.script {
   name = "nix-update-file";
   package = pkgs.writers.writeNuBin "nix-update-file" /* nu */ ''
+
     # Update a standalone Nix package file (not in nixpkgs or a flake).
-    def main [file: string] {
+    def main [
+      --unstable # Update to latest commit of branch.
+      file: string # Nix file to update.
+    ] {
         let fullpath =  $file | path expand
 
         print $"Updating ($fullpath)"
@@ -21,7 +25,9 @@ delib.script {
             }
         " | save --raw --force /tmp/nix-update.nix
 
-        nix-update -f /tmp/nix-update.nix --override-filename "$FILE" file-package
+        let extra_args = if $unstable { [--version=branch] } else { [] }
+
+        nix-update -f /tmp/nix-update.nix --override-filename "$FILE" ...$extra_args file-package
     }
   '';
 }
