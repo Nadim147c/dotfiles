65cc93c9cf709a93e99484d3774174eb6acef61f
Ephemeral <theephemeral.txt@gmail.com>
2025-12-28 21:30:36 +0600

    dev/nix: add statix package

--- modules/dev/nix.nix
+++ modules/dev/nix.nix
@@ -4,6 +4,21 @@
   pkgs,
   ...
 }:
+let
+  statix = pkgs.statix.overrideAttrs (_: rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "oppiliappan";
+      repo = "statix";
+      rev = "43681f0da4bf1cc6ecd487ef0a5c6ad72e3397c7";
+      hash = "sha256-LXvbkO/H+xscQsyHIo/QbNPw2EKqheuNjphdLfIZUv4=";
+    };
+
+    cargoDeps = pkgs.rustPlatform.importCargoLock {
+      lockFile = src + "/Cargo.lock";
+      allowBuiltinFetchGit = true;
+    };
+  });
+in
 delib.module {
   name = "dev.nix";
 
@@ -13,5 +28,6 @@ delib.module {
     nix-update
     nixd
     nixfmt-rfc-style
+    statix
   ];
 }
