2913616de74e69471086a8c95b26c51525c04c99
Ephemeral <theephemeral.txt@gmail.com>
2025-12-28 21:28:41 +0600

    git: update git-copy script

--- modules/programs/git/git-copy.nix
+++ modules/programs/git/git-copy.nix
@@ -3,26 +3,59 @@
   pkgs,
   ...
 }:
-delib.script {
+let
+  url-parser = pkgs.writers.writeNu "git-copy-nu" /* nu */ ''
+    def getpath [u: string] {
+      $u | path split | skip 1 | first 2 | str join /
+    }
+
+    def main [u: string] {
+      let url = $u | url parse
+      let host = $url.host
+
+      let out = match $host {
+        github.com | gitlab.com => {
+          let path = getpath $url.path
+          echo ({
+            url: $"git@($host):($path)"
+            path: $path
+          })
+        }
+        aur.archlinux.org => {
+          let path = $url.path | path split | last | str replace --regex '.git$' ""
+          echo ({
+            url: $"ssh://aur@($host)/($path).git"
+            path: $"aur/($path)"
+          })
+        }
+        _ => {
+          error make -u { msg: "Unsupported url host. use `git clone <url>` instead!" }
+        }
+      }
+      return ($out | to json --raw)
+    }
+  '';
+in
+delib.script rec {
   name = "git-copy";
   partof = "programs.git";
-  package = pkgs.writers.writeNuBin "git-copy" /* nu */ ''
-    # Clone GitHub repository with proper name
-    def main [
-        url: string, # url of the git repository
-        ...params: string # extra git options
-    ] {
-        let u = $url | url parse
-        let p = $u | get path | path split
-        let user = $p | get 1
-        let repo = $p | get 2
+  package = pkgs.writeShellScriptBin name /* nu */ ''
+    if [[ $# -eq 0 ]]; then
+        echo "Use: ${name} <url> ...<git-flags>" >&2
+        exit 1
+    fi
 
-        if ($u.host != "github.com" or $user == "Nadim147c") {
-            git clone $url ...$params
-            return
-        }
+    url=$(${url-parser} "$1")
+    shift
 
-        git clone $"git@github.com:($user)/($repo)" ...$params $"($user):($repo)"
-    }
+    uri=$(echo "$url" | jq -r .url)
+    dir=$(echo "$url" | jq -r .path)
+
+    username=$(gh auth status --active --jq '.[][][].login' --json hosts || echo "")
+    if [[ -n "$username" ]]; then
+        dir=$(echo "$dir" | sed "s|$username/||")
+    fi
+
+    git clone "$uri" "$HOME/git/$dir" "$@"
   '';
 }
