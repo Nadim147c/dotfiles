b126b89f47d031083fb34bac0715290001a809a8
Ephemeral <theephemeral.txt@gmail.com>
2025-12-28 21:30:05 +0600

    audio: update nix format

--- modules/toplevel/audio.nix
+++ modules/toplevel/audio.nix
@@ -4,11 +4,11 @@ delib.module {
 
   options = delib.singleEnableOption true;
 
-  nixos.ifEnabled.services = {
-    pipewire.enable = true;
-    pipewire.pulse.enable = true; # replace PulseAudio
-    pipewire.jack.enable = true; # replace JACK
-    pipewire.alsa.enable = true; # replace ALSA
-    pipewire.alsa.support32Bit = true; # for 32-bit apps (e.g., Steam)
+  nixos.ifEnabled.services.pipewire = {
+    enable = true;
+    pulse.enable = true; # replace PulseAudio
+    jack.enable = true; # replace JACK
+    alsa.enable = true; # replace ALSA
+    alsa.support32Bit = true; # for 32-bit apps (e.g., Steam)
   };
 }
