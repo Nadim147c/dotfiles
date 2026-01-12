a8252b056d04ea4473c2aa2fa166d35cae733629
Ephemeral <theephemeral.txt@gmail.com>
2026-01-12 15:02:28 +0600

    overlays/svu: update the svu package

--- overlays/svu.nix
+++ overlays/svu.nix
@@ -2,7 +2,17 @@
 delib.overlayModule {
   name = "svu";
   overlay = final: prev: {
-    svu = prev.svu.overrideAttrs (old: {
+    svu = prev.svu.overrideAttrs (old: rec {
+      version = "3.3.0";
+
+      src = prev.fetchFromGitHub {
+        owner = "caarlos0";
+        repo = "svu";
+        rev = "v${version}";
+        sha256 = "sha256-3Rj+2ROo9TuWc2aZ8kkGeXH+PHjKva6nD7wlXHY/LQg=";
+      };
+
+      vendorHash = "sha256-2QznJ28lp/+f4MIbu4Wi5Kx46B7IIHGYGofY7B1OEjo=";
       nativeBuildInputs = old.nativeBuildInputs ++ [ prev.installShellFiles ];
       postInstall =
         let
