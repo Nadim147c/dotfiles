3abac8e5867fb69deead7b81a25c7fb847af09e4
Ephemeral <theephemeral.txt@gmail.com>
2026-01-12 22:01:14 +0600

    thunar: update thunar config

--- modules/programs/thunar.nix
+++ modules/programs/thunar.nix
@@ -1,22 +1,23 @@
 {
   delib,
+  host,
   pkgs,
   ...
 }:
 delib.module {
   name = "programs.thunar";
-  options = delib.singleEnableOption false;
+  options = delib.singleEnableOption host.guiFeatured;
   nixos.ifEnabled = {
     services.gvfs.enable = true;
     services.tumbler.enable = true;
 
     programs.thunar = {
       enable = true;
-      plugins = with pkgs.xfce; [
+      plugins = with pkgs; [
+        ffmpegthumbnailer
         thunar-archive-plugin
         thunar-media-tags-plugin
         thunar-volman
-        pkgs.ffmpegthumbnailer
       ];
     };
   };
