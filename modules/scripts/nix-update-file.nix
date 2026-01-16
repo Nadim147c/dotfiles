93c7135d72e54de1db6578d85a5931d18ffff938
Ephemeral <theephemeral.txt@gmail.com>
2026-01-16 16:44:55 +0600

    scripts/nix-update-file: fix parameter and add completion

--- modules/scripts/nix-update-file.nix
+++ modules/scripts/nix-update-file.nix
@@ -3,31 +3,41 @@
   pkgs,
   ...
 }:
-delib.script {
+delib.script rec {
   name = "nix-update-file";
-  package = pkgs.writers.writeNuBin "nix-update-file" /* nu */ ''
-
-    # Update a standalone Nix package file (not in nixpkgs or a flake).
-    def main [
-      --unstable # Update to latest commit of branch.
-      file: string # Nix file to update.
-    ] {
+  completion = {
+    inherit name;
+    flags = {
+      "--unstable" = "Use latest commit of current branch";
+    };
+    completion.positional = [ "$files" ];
+  };
+  package = pkgs.writeNuApplication {
+    inherit name;
+    runtimeInputs = with pkgs; [ nix-update ];
+    text = /* nu */ ''
+      # Update a standalone Nix package file (not in nixpkgs or a flake).
+      def main [
+        --unstable # Update to latest commit of branch.
+        file: string # Nix file to update.
+      ] {
         let fullpath =  $file | path expand
 
         print $"Updating ($fullpath)"
 
         echo $"
-            {
-              pkgs ? import <nixpkgs> { },
-            }:
-            rec {
-              file-package = pkgs.callPackage ($fullpath) { };
-            }
+        {
+          pkgs ? import <nixpkgs> { },
+        }:
+        rec {
+          file-package = pkgs.callPackage ($fullpath) { };
+        }
         " | save --raw --force /tmp/nix-update.nix
 
         let extra_args = if $unstable { [--version=branch] } else { [] }
 
-        nix-update -f /tmp/nix-update.nix --override-filename "$FILE" ...$extra_args file-package
-    }
-  '';
+        nix-update -f /tmp/nix-update.nix --override-filename $file ...$extra_args file-package
+      }
+    '';
+  };
 }
