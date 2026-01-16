ba12fb15f85f3c64a076f00dc6e9c299dc88c844
Ephemeral <theephemeral.txt@gmail.com>
2026-01-16 16:44:55 +0600

    git: move semver script to writeNuApplication

--- modules/programs/git/semver.nix
+++ modules/programs/git/semver.nix
@@ -1,18 +1,8 @@
 {
   delib,
   pkgs,
-  lib,
   ...
 }:
-let
-  inherit (lib) escapeShellArgs;
-
-  runtimeInputs = with pkgs; [
-    gum
-    svu
-  ];
-  PATH = map (x: "${x}/bin") runtimeInputs |> escapeShellArgs;
-in
 delib.script rec {
   name = "git-semver";
   partof = "programs.git";
@@ -24,33 +14,38 @@ delib.script rec {
       { name = "prerelease"; }
     ];
   };
-  package = pkgs.writers.writeNuBin name /* nu */ ''
-    $env.PATH = [ ${PATH} ] ++ $env.PATH
+  package = pkgs.writeNuApplication {
+    inherit name;
+    runtimeInputs = with pkgs; [
+      gum
+      svu
+    ];
+    text = /* nu */ ''
+      def main [] { }
 
-    def main [] { }
-
-    def 'main current' [] {
-      svu current
-    }
-
-    def 'main next' [] {
-      let version = svu next
-      print $version
-      gum confirm $"Do want to create new teg? \(($version)\)"
-      git tag $version
-    }
-
-    def 'main prerelease' [] {
-      let current = svu current --json | from json
-      if "prerelease" not-in $current  {
-        print --stderr "Current version does not have any pre-release!"
-        exit 1
+      def 'main current' [] {
+        svu current
       }
-      let next = $current | upsert build {|old| $old.build | default "0" | into int | $in + 1 }
-      let version = $next | format pattern "{major}.{minor}.{patch}-{prerelease}.{build}"
-      print $version
-      gum confirm $"Do want to create new teg? \(($version)\)"
-      git tag $version
-    }
-  '';
+
+      def 'main next' [] {
+        let version = svu next
+        print $version
+        gum confirm $"Do want to create new teg? \(($version)\)"
+        git tag $version
+      }
+
+      def 'main prerelease' [] {
+        let current = svu current --json | from json
+        if "prerelease" not-in $current  {
+          print --stderr "Current version does not have any pre-release!"
+          exit 1
+        }
+        let next = $current | upsert build {|old| $old.build | default "0" | into int | $in + 1 }
+        let version = $next | format pattern "{major}.{minor}.{patch}-{prerelease}.{build}"
+        print $version
+        gum confirm $"Do want to create new teg? \(($version)\)"
+        git tag $version
+      }
+    '';
+  };
 }
