a7c26deedecc6807a9b5763ea63a69618200f03a
Ephemeral <theephemeral.txt@gmail.com>
2026-01-27 11:02:16 +0600

    jujutsu: enable verbose commit message

--- modules/programs/jujutsu.nix
+++ modules/programs/jujutsu.nix
@@ -21,6 +21,22 @@ delib.module {
           backend = "ssh";
           key = "~/.ssh/master.pub";
         };
+        templates.draft_commit_description = ''
+          concat(
+            description,
+            surround(
+              "\nJJ: This commit contains the following changes:\n", "",
+              indent("JJ:     ", diff.summary()),
+            ),
+            surround(
+              "\nJJ: This commit contains the following changes:\n", "",
+              indent("JJ:     ", diff.stat(72)),
+            ),
+            "\n",
+            "JJ: ignore-rest\n",
+            diff.git(),
+          )
+        '';
       };
     };
   };
